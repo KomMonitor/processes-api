46aedb175a7f228e9d417e66fb06f4c98e5c1116
cDanowski <christian.danowski-buhren@hs-bochum.de>
2025-06-23 13:33:22 +0200

    fix two test processes to make GET /processes endpoint work

--- processor/process/kommonitor/hello_world.py
+++ processor/process/kommonitor/hello_world.py
@@ -1,15 +1,50 @@
-from typing import Dict, Optional
+import json
+from typing import Tuple
+import logging
+import math
 
+import openapi_client
 from openapi_client import ApiClient
-from openapi_client.rest import ApiException
+from prefect import task, flow
+from prefect.cache_policies import NO_CACHE
 from pygeoapi.process.base import *
 from pygeoapi.util import JobStatus
+from pygeoapi_prefect import schemas
+from pygeoapi_prefect.schemas import ProcessDescription, ProcessJobControlOption, Parameter, \
+    AdditionalProcessIOParameters
+from pygeoapi_prefect.schemas import ProcessInput, ProcessIOSchema, ProcessIOType
 
-from ..base import KommonitorProcess, KommonitorProcessConfig
-from pygeoapi_prefect.schemas import ProcessInput, ProcessDescription, ProcessIOType, ProcessIOSchema, ProcessJobControlOption, Parameter, AdditionalProcessIOParameters, OutputExecutionResultInternal, ProcessOutput
+# from ..base import DataManagementException
 
+try:
+    from .. import pykmhelper
+except ImportError:
+    from processor.process import pykmhelper
+
+try:
+    from ..pykmhelper import IndicatorType, IndicatorCollection, IndicatorCalculationType
+except ImportError:
+    from processor.process.pykmhelper import IndicatorType, IndicatorCollection, IndicatorCalculationType
+
+try:
+    from ..base import KommonitorProcess, KommonitorProcessConfig, KommonitorResult, DataManagementException, \
+        KommonitorJobSummary, KOMMONITOR_DATA_MANAGEMENT_URL, generate_flow_run_name
+except ImportError:
+    from processor.process.base import KommonitorProcess, KommonitorProcessConfig, KommonitorResult, DataManagementException, \
+        KommonitorJobSummary, KOMMONITOR_DATA_MANAGEMENT_URL, generate_flow_run_name
+
+
+@flow(persist_result=True, name="hello_world", flow_run_name=generate_flow_run_name)
+def process_flow(
+        job_id: str,
+        execution_request: schemas.ExecuteRequest
+) -> dict:
+    return KommonitorProcess.execute_process_flow(HelloWorld.run, job_id, execution_request)
 
 class HelloWorld(KommonitorProcess):
+
+    process_flow = process_flow
+
     detailed_process_description = ProcessDescription(
         id="hello-world",
         version="1.0.0",
@@ -39,20 +74,15 @@ class HelloWorld(KommonitorProcess):
                 schema=ProcessIOSchema(type=ProcessIOType.STRING)
             ),
         },
-        outputs={
-            "result": ProcessOutput(
-                schema=ProcessIOSchema(
-                    type=ProcessIOType.STRING,
-                    contentMediaType="text/plain",
-                )
-            )
-        },
+        outputs = KommonitorProcess.common_output
     )
 
+    @staticmethod
+    @task(cache_policy=NO_CACHE)
     def run(self,
             config: KommonitorProcessConfig,
             logger: logging.Logger,
-            data_management_client: ApiClient) -> (JobStatus, Optional[Dict[str, OutputExecutionResultInternal]]):
+            data_management_client: ApiClient) -> Tuple[JobStatus, KommonitorResult, KommonitorJobSummary]:
 
         # 1. Load inputs
         inputs = config.inputs
