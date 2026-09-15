88487b3c61ab7870394c36156167b7c2ff413b02
nilskoepke <n.koepke@52north.org>
2026-09-15 12:00:14 +0200

    Revert "add user_id to schedule inputs"
    This reverts commit 233c75ac80e713853a1d217eac4dfa6455b04b50.

--- processor/process/custom/km_processes.py
+++ processor/process/custom/km_processes.py
@@ -2,7 +2,6 @@ from authlib.oauth2.rfc6750 import InsufficientScopeError
 from pygeoapi.api import (
     SYSTEM_LOCALE, apply_gzip
 )
-import jwt
 import json
 import logging
 import urllib.parse
@@ -101,11 +100,6 @@ def schedule_process(api: API, request: APIRequest,
             request.format, 'NoSuchProcess', msg)
 
     data = request.data
-
-    subject_token = request.headers["Authorization"].split(" ")[1]
-    decoded = jwt.decode(subject_token, options={"verify_signature": False})
-    user_id = decoded.get("sub")
-
     if not data:
         # TODO not all processes require input, e.g. time-dependent or
         #      random value generators
@@ -131,7 +125,6 @@ def schedule_process(api: API, request: APIRequest,
             'InvalidParameterValue', msg)
 
     data_dict = data.get('inputs', {})
-    data_dict['user_id'] = user_id
     logger.debug(data_dict)
 
     requested_outputs = data.get('outputs')
@@ -349,8 +342,6 @@ def get_schedules(api: API, request: APIRequest, schedule_id=None) -> Tuple[dict
     for schedule_ in schedules:
         logger.debug("schedule data model:")
         logger.debug(schedule_)
-
-        print(schedule_)
         schedule2 = {
             'type': 'process',
             'processID': schedule_['process_id'],
